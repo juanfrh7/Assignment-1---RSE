eb6c7c7d8609ef3351b69fdf8c7a409c91e4dc6c
Juan Francisco <juanfrh7@gmail.com>
2021-11-21 19:01:58 +0000

    define the haversine distance function

--- utils.py
+++ utils.py
@@ -6,4 +6,13 @@ def haversine_distance(lat1, lon1, lat2, lon2):
 
     Latitude and longitude for each point are given in degrees.
     """
+    lat_dif = radians(lat2-lat1)  #latitude difference
+    lon_dif = radians(lon2-lon1)  #longitude difference
     
+    #calculate the distance
+    a = sin(lat_dif/2) * sin(lat_dif/2) + cos(radians(lat1)) \
+        * cos(radians(lat2)) * sin(lon_dif/2) * sin(lon_dif/2)
+    
+    c = 6371 * 2 * atan2(sqrt(a), sqrt(1-a))
+
+    return c
