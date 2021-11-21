26e2175b7726c6f17a298882b9ff4a83c1bd87e6
Juan Francisco <juanfrh7@gmail.com>
2021-11-21 19:01:11 +0000

    import all the libraries and modules

--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
+import math
+from math import radians, sin, cos, sqrt, atan2
+
 def haversine_distance(lat1, lon1, lat2, lon2):
     """Return the distance in km between two points around the Earth.
 
     Latitude and longitude for each point are given in degrees.
     """
-    raise NotImplementedError
+    
