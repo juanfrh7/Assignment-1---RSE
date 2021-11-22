f3ad5cfb9a12efafb7069c2eb199dfbca1207b32
Juan Francisco <juanfrh7@gmail.com>
2021-11-22 00:04:46 +0000

    define the test add mass balance function

--- test_glaciers.py
+++ test_glaciers.py
@@ -2,6 +2,10 @@ import pytest
 import glaciers
 from glaciers import Glacier, GlacierCollection
 
+location = ''
+filepath1 = Path(location + 'sheet-A.csv')
+filepath2 = Path(location + 'sheet-EE.csv')
+
 def test_error_messages():
     #define variables
     glacier_id = '17363'
@@ -23,7 +27,12 @@ def test_error_messages():
     assert example.code == code
 
 def test_add_mass_balance():
-    raise NotImplementedError
+    #create a collection
+    collection = GlacierCollection(filepath1)
+    collection.read_mass_balance_data(filepath2)
+
+    #check for the glacier Agua Negra which has partial and total measurements for 2018 and 2019
+    assert collection.glacier_classes[0].mass_balance == [-793.0, -418.0, 332.0, -7886.0, -2397.0, -13331.0]
 
 def test_filter_code():
     raise NotImplementedError
